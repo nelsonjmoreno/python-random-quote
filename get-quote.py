63788e5ee2be853dfaf3a8d68e1588d4e9229264
Nelson <nel.j.moreno@gmail.com>
2020-06-24 23:37:38 +0000

    Will now generate random number based on the number of quotes in quotes.txt. Can freely add and remove quotes.

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,21 @@
 import random
 
-n = random.randint(0,13)
+mainfile = "quotes.txt"
+
+def file_length():
+  with open(mainfile) as c:
+    for i, l in enumerate(c):
+      pass
+  return i 
 
 def primary():
   #print("Keep it logically awesome.")
-
-  f = open("quotes.txt")
+  f = open(mainfile)
   quotes = f.readlines()
   f.close()
+  n = random.randint(0, file_length())
 
   print(quotes[n])
-
 
 if __name__== "__main__":
   primary()

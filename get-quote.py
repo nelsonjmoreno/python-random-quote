d2d2e929287d620947c7534710408275a243510e
Nelson <nel.j.moreno@gmail.com>
2020-06-25 00:16:07 +0000

    Prints out 2 random quotes. Second quote cannot be the first quote. Newlines have been stripped.

--- get-quote.py
+++ get-quote.py
@@ -1,21 +1,27 @@
 import random
 
+#Defines primary quote file
 mainfile = "quotes.txt"
 
+#Obtains count of the number of quotes in quotes.txt
 def file_length():
   with open(mainfile) as c:
     for i, l in enumerate(c):
       pass
   return i 
 
+#Prints 2 random quotes
 def primary():
-  #print("Keep it logically awesome.")
   f = open(mainfile)
   quotes = f.readlines()
   f.close()
-  n = random.randint(0, file_length())
+  n1 = random.randint(0, file_length())
+  n2 = random.randint(0, file_length())
+  if n2 == n1:
+    next(n2)
 
-  print(quotes[n])
+  print(quotes[n1].strip())
+  print(quotes[n2].strip())
 
 if __name__== "__main__":
   primary()
